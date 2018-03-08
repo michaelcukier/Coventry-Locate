116b0ec3739102b6af01fb66a7d22821f84ba0d6
Michael Cukier <33379581+michaelcukier@users.noreply.github.com>
2018-03-08 14:16:10 +0000

    Add files via upload

--- CovLoc/CovLoc/__init__.py
+++ CovLoc/CovLoc/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template
+import MySQLdb
 
 app = Flask(__name__)
 
+db = MySQLdb.connect(host="localhost",  # your host
+                     user="root",  # username
+                     passwd="cukier",  # password
+                     db="testbruh")  # name of the database
+
 @app.route('/')
 def homepage():
-    return render_template("Draft2.html")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM buildings")
+    array_of_buildings = []
+    for row in cur.fetchall():
+        array_of_buildings.append(row[1])
 
-@app.route('/hello/<name>')
-def hello(name):
-
-    if name == "Wiktor":
-        return "FUCK YOU"
-    elif name == "Michael":
-        return "I LOVE YOU"
+    return render_template("index.html", array_of_buildings=array_of_buildings)
 
 if __name__ == "__main__":
     app.run()
